c06697854e45766e1abca3a541be8ce89d5b9020
Sadrach Martinez <63802470+sadrachm@users.noreply.github.com>
2021-08-13 17:22:37 -0700

    Add environment Variables

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+
 
 mongoose.connect("mongodb://localhost:27017/userDB",
  {useNewUrlParser:true, useUnifiedTopology: true }
 );
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
